5ae6695d5050d8fd96d126520d407c18adb33727
Kishan Gosrani <kishan.gosrani22@gmail.com>
2022-01-29 02:26:50 +0800

    I have updated the comments of the html

--- Assets/js/02_ListedCocktails.js
+++ Assets/js/02_ListedCocktails.js
@@ -1,51 +1,91 @@
 const container = document.getElementById('cocktail-cards');
 
+//this is the fetch request which will derive search results for the cocktail receipes using a public API listed below
+
 function getDrinkApi(drinkName) {
   const drinkApi = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName;
-    //this is the fetch request will get us the search results for the cocktail and meal receipes
 
-    fetch(drinkApi)
-      .then(function (response) {
-        return response.json();
-      })
-      .then (function (data){ 
-        console.log(data);
-        for (var i = 0; i < data.drinks.length; i++) {
-          var item = data.drinks[i];
-          var divEl = document.createElement("div");
-            divEl.setAttribute("class", "tile is-2 is-parent");
-            
-            var sectionEl = document.createElement("section");
-            sectionEl.setAttribute("class", "tile is-child box");
 
-            var pEl = document.createElement("p");
-            pEl.setAttribute("class", "title is-size-7 has-text-centered");
-            pEl.innerHTML = item.strDrink;
+  //Fetches information and converts the data (response) into JSON which is a string form of data the system can interpret to promise an answer to the user
 
-            var imgEl = document.createElement("img");
-            imgEl.setAttribute("src", item.strDrinkThumb);
+  fetch(drinkApi)
+    .then(function (response) {
+      return response.json();
+    })
 
-            var ulEl = document.createElement('ul');
+    //once the data is transferred the function now begins a loop to create the cocktail cards depending on the number of coctails or results available
+    // (if 6 cocktails of same name) then the loop will create 6 cards)
 
-            var detailBtnEl = document.createElement("button");
-            detailBtnEl.setAttribute("class", "button is-info");
-            detailBtnEl.innerHTML = " more ";
+    .then(function (data) {
+      console.log(data);
 
-            //<button id="searchButton" class="button is-info"> Search </button>
+      for (var i = 0; i < data.drinks.length; i++) {
 
-            // display to the html
-            container.appendChild(divEl);
-            divEl.appendChild(sectionEl);
-            sectionEl.appendChild(pEl);
-            sectionEl.appendChild(imgEl);
-            sectionEl.appendChild(ulEl);
+        // this is the length of i in the loop (like explained above -> (if 6 cocktails containing keyword search name) then the loop will create 6 cards))
 
-            //for (var i = 1; i < 8; i++) {
-            //  createLi(ulEl, item["strIngredient" + i], item["strMeasure" + i]);
-            // }
+        var item = data.drinks[i];
 
-            //* pls decide if this should be here or the next page
-            sectionEl.innerHTML += `
+        //creat a divEl and set its arritubes to "class" named tile is-2 is-parent (which is the css framework method of bulma)
+
+        var divEl = document.createElement("div");
+
+        divEl.setAttribute("class", "tile is-2 is-parent");
+
+        // thereafter within the div elemnt create a section element and set its arritubes to "class" named tile is-child box
+
+        var sectionEl = document.createElement("section");
+
+        sectionEl.setAttribute("class", "tile is-child box");
+
+        // inside the section, the loop wil creat a h2 element which will contain the name of the of the cocktail and this name is derived from the item.strDrink of API 
+
+        var h2El = document.createElement("h2");
+
+        h2El.setAttribute("class", "title is-size-7 has-text-centered");
+
+        h2El.innerHTML = item.strDrink;
+
+        // After the heading of the cocktail we will display the image of the cocktail by creating a image element and using the source of this image from item.strDrinkThumb which is in the API information
+
+        var imgEl = document.createElement("img");
+
+        imgEl.setAttribute("src", item.strDrinkThumb);
+
+        // the loop will then create a UL Elment which will include a list (li) of ingredients that the cocktail has, e.g. tequila, triple sec, lime juice.
+
+        var ulEl = document.createElement('ul');
+
+        // create a button named "more" so the user can click this button and get more information regarding this drink
+
+        var detailBtnEl = document.createElement("button");
+
+        detailBtnEl.setAttribute("class", "button is-info");
+
+        detailBtnEl.innerHTML = " more ";
+
+        // display to the html
+        //container is the whole container named cocktail cards, and we must append the elements we created so it can appear on our html successfully
+        container.appendChild(divEl);
+        divEl.appendChild(sectionEl);
+        sectionEl.appendChild(h2El);
+        sectionEl.appendChild(imgEl);
+        sectionEl.appendChild(ulEl);
+
+        // for (var i = 1; i < 8; i++) {
+        //   createLi(ulEl, item["strIngredient" + i], item["strMeasure" + i]);
+        // }
+
+        // function createLi(ulElInput, ingrdientName, measure) {
+        //   if (!ingrdientName || !measure) {
+        //     return;
+        //   }
+        //   var li = document.createElement('li');
+        //   li.innerHTML = `<strong>${ingrdientName} </strong><span class="has-text-danger">${measure}<span>`;
+        //   ulElInput.appendChild(li);
+        // }
+
+        //* pls decide if this should be here or the next page
+        ulEl.innerHTML += `
                               <li><strong>${item.strIngredient1} </strong><span class="has-text-danger">${item.strMeasure1}<span></li>
                               <li><strong>${item.strIngredient2} </strong><span class="has-text-danger">${item.strMeasure2}<span></li>
                               <li><strong>${item.strIngredient3} </strong><span class="has-text-danger">${item.strMeasure3}<span></li>
@@ -53,25 +93,22 @@ function getDrinkApi(drinkName) {
                               <li><strong>${item.strIngredient5} </strong><span class="has-text-danger">${item.strMeasure5}<span></li>
                               <li><strong>${item.strIngredient6} </strong><span class="has-text-danger">${item.strMeasure6}<span></li>
                               <li><strong>${item.strIngredient7} </strong><span class="has-text-danger">${item.strMeasure7}<span></li>`;
-                
-            detailBtnEl.addEventListener('click', function(event){
-                   document.location = "./03_Detail.html?id=" + item.idDrink;
-            });
-            sectionEl.appendChild(detailBtnEl);
-        } 
-      });
+            
+        //add an event listener to the detailBtnEl we created above named "more"
+        //when user clicks this button the document.location changes to "./03_Detail.html?id=" + item.idDrink;
+        // the item.idDrink is then used to navigate to the next API which is getting in-depth information using the ID of the drink
+
+        detailBtnEl.addEventListener('click', function (event) {
+          document.location = "./03_Detail.html?id=" + item.idDrink;
+        });
+        //detailBtnEl is then appended to be considered part of the sectionEl (cocktail-cards)
+        sectionEl.appendChild(detailBtnEl);
+      }
+    });
 }
 
-//function createLi(ulElInput, ingrdientName, measure) {
-//  if (!ingrdientName || !measure) {
-//    return;
-//  }
+//get the drinkname and split it from the 2nd poition (0,1) and use this split tpo include the name the user searched 
+var drinkName = document.location.search.split("=")[1];
 
-//  var li = document.createElement('li');
-//              li.innerHTML = `<strong>${ingrdientName} </strong><span class="has-text-danger">${measure}<span>`;
-//              ulElInput.appendChild(li);
-//}
-
-  var drinkName = document.location.search.split("=")[1];
-
-  getDrinkApi(drinkName);
+//runs the whole loop and this input becomes the drinkName and the whole function is repeated again
+getDrinkApi(drinkName);
