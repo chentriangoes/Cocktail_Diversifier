38fdf35ae82922b8939b04c36deb41e0329ff022
chentriangoes <chentriangoes@gmail.com>
2022-01-29 11:00:25 +1100

    YC's commit_Listed Ingredients

--- Assets/js/02_ListedCocktails.js
+++ Assets/js/02_ListedCocktails.js
@@ -53,7 +53,7 @@ function getDrinkApi(drinkName) {
 
         // the loop will then create a UL Elment which will include a list (li) of ingredients that the cocktail has, e.g. tequila, triple sec, lime juice.
 
-        var ulEl = document.createElement('ul');
+        var ulElInput = document.createElement('ul');
 
         // create a button named "more" so the user can click this button and get more information regarding this drink
 
@@ -69,7 +69,7 @@ function getDrinkApi(drinkName) {
         divEl.appendChild(sectionEl);
         sectionEl.appendChild(h2El);
         sectionEl.appendChild(imgEl);
-        sectionEl.appendChild(ulEl);
+        sectionEl.appendChild(ulElInput);
 
         // for (var i = 1; i < 8; i++) {
         //   createLi(ulEl, item["strIngredient" + i], item["strMeasure" + i]);
@@ -84,15 +84,107 @@ function getDrinkApi(drinkName) {
         //   ulElInput.appendChild(li);
         // }
 
+        var ingredient1li = document.createElement('li');
+        var ingredient2li = document.createElement('li');
+        var ingredient3li = document.createElement('li');
+        var ingredient4li = document.createElement('li');
+        var ingredient5li = document.createElement('li');
+        var ingredient6li = document.createElement('li');
+        var ingredient7li = document.createElement('li');
+        var ingredient8li = document.createElement('li');
+        var ingredient9li = document.createElement('li');
+        var ingredient10li = document.createElement('li');
+        var ingredient11li = document.createElement('li');
+        var ingredient12li = document.createElement('li');
+        var ingredient13li = document.createElement('li');
+        var ingredient14li = document.createElement('li');
+        var ingredient15li = document.createElement('li');
+
+        ingredient1li.innerHTML = `<strong>${item.strIngredient1} </strong><span class="has-text-danger">${item.strMeasure1}<span>`;
+        ingredient2li.innerHTML = `<strong>${item.strIngredient2} </strong><span class="has-text-danger">${item.strMeasure2}<span>`;
+        ingredient3li.innerHTML = `<strong>${item.strIngredient3} </strong><span class="has-text-danger">${item.strMeasure3}<span>`;
+        ingredient4li.innerHTML = `<strong>${item.strIngredient4} </strong><span class="has-text-danger">${item.strMeasure4}<span>`;
+        ingredient5li.innerHTML = `<strong>${item.strIngredient5} </strong><span class="has-text-danger">${item.strMeasure5}<span>`;
+        ingredient6li.innerHTML = `<strong>${item.strIngredient6} </strong><span class="has-text-danger">${item.strMeasure6}<span>`;
+        ingredient7li.innerHTML = `<strong>${item.strIngredient7} </strong><span class="has-text-danger">${item.strMeasure7}<span>`;
+        ingredient8li.innerHTML = `<strong>${item.strIngredient8} </strong><span class="has-text-danger">${item.strMeasure8}<span>`;
+        ingredient9li.innerHTML = `<strong>${item.strIngredient9} </strong><span class="has-text-danger">${item.strMeasure9}<span>`;
+        ingredient10li.innerHTML = `<strong>${item.strIngredient10} </strong><span class="has-text-danger">${item.strMeasure10}<span>`;
+        ingredient11li.innerHTML = `<strong>${item.strIngredient11} </strong><span class="has-text-danger">${item.strMeasure11}<span>`;
+        ingredient12li.innerHTML = `<strong>${item.strIngredient12} </strong><span class="has-text-danger">${item.strMeasure12}<span>`;
+        ingredient13li.innerHTML = `<strong>${item.strIngredient13} </strong><span class="has-text-danger">${item.strMeasure13}<span>`;
+        ingredient14li.innerHTML = `<strong>${item.strIngredient14} </strong><span class="has-text-danger">${item.strMeasure14}<span>`;
+        ingredient15li.innerHTML = `<strong>${item.strIngredient15} </strong><span class="has-text-danger">${item.strMeasure15}<span>`;
+
+        if (item.strIngredient1 && item.strMeasure1){
+          ulElInput.appendChild(ingredient1li)
+        }
+
+        if (item.strIngredient2 && item.strMeasure2){
+          ulElInput.appendChild(ingredient2li)
+        }
+
+        if (item.strIngredient3 && item.strMeasure3){
+          ulElInput.appendChild(ingredient3li)
+        }
+
+        if (item.strIngredient4 && item.strMeasure4){
+          ulElInput.appendChild(ingredient4li)
+        }
+
+        if (item.strIngredient5 && item.strMeasure5){
+          ulElInput.appendChild(ingredient5li)
+        }
+
+        if (item.strIngredient6 && item.strMeasure6){
+          ulElInput.appendChild(ingredient6li)
+        }
+
+        if (item.strIngredient7 && item.strMeasure7){
+          ulElInput.appendChild(ingredient7li)
+        }
+
+        if (item.strIngredient8 && item.strMeasure8){
+          ulElInput.appendChild(ingredient8li)
+        }
+
+        if (item.strIngredient9 && item.strMeasure9){
+          ulElInput.appendChild(ingredient9li)
+        }
+
+        if (item.strIngredient10 && item.strMeasure01){
+          ulElInput.appendChild(ingredient10li)
+        }
+
+        if (item.strIngredient11 && item.strMeasure11){
+          ulElInput.appendChild(ingredient11li)
+        }
+
+        if (item.strIngredient12 && item.strMeasure12){
+          ulElInput.appendChild(ingredient12li)
+        }
+
+        if (item.strIngredient13 && item.strMeasure13){
+          ulElInput.appendChild(ingredient13li)
+        }
+
+        if (item.strIngredient14 && item.strMeasure14){
+          ulElInput.appendChild(ingredient14li)
+        }
+
+        if (item.strIngredient15 && item.strMeasure15){
+          ulElInput.appendChild(ingredient15li)
+        }
+         
         //* pls decide if this should be here or the next page
-        ulEl.innerHTML += `
-                              <li><strong>${item.strIngredient1} </strong><span class="has-text-danger">${item.strMeasure1}<span></li>
-                              <li><strong>${item.strIngredient2} </strong><span class="has-text-danger">${item.strMeasure2}<span></li>
-                              <li><strong>${item.strIngredient3} </strong><span class="has-text-danger">${item.strMeasure3}<span></li>
-                              <li><strong>${item.strIngredient4} </strong><span class="has-text-danger">${item.strMeasure4}<span></li>
-                              <li><strong>${item.strIngredient5} </strong><span class="has-text-danger">${item.strMeasure5}<span></li>
-                              <li><strong>${item.strIngredient6} </strong><span class="has-text-danger">${item.strMeasure6}<span></li>
-                              <li><strong>${item.strIngredient7} </strong><span class="has-text-danger">${item.strMeasure7}<span></li>`;
+        //ulEl.innerHTML += `
+        //                      <li><strong>${item.strIngredient1} </strong><span class="has-text-danger">${item.strMeasure1}<span></li>
+        //                      <li><strong>${item.strIngredient2} </strong><span class="has-text-danger">${item.strMeasure2}<span></li>
+        //                      <li><strong>${item.strIngredient3} </strong><span class="has-text-danger">${item.strMeasure3}<span></li>
+        //                      <li><strong>${item.strIngredient4} </strong><span class="has-text-danger">${item.strMeasure4}<span></li>
+        //                      <li><strong>${item.strIngredient5} </strong><span class="has-text-danger">${item.strMeasure5}<span></li>
+        //                      <li><strong>${item.strIngredient6} </strong><span class="has-text-danger">${item.strMeasure6}<span></li>
+        //                      <li><strong>${item.strIngredient7} </strong><span class="has-text-danger">${item.strMeasure7}<span></li>`;
             
         //add an event listener to the detailBtnEl we created above named "more"
         //when user clicks this button the document.location changes to "./03_Detail.html?id=" + item.idDrink;
